97a722ea23f04087f702b3454a044d6e0e08228e
xinjiezhu <xinjiezhu@foxmail.com>
2014-03-23 16:35:00 +0800

    exercise1

---
+++ exercise1.cpp
@@ -0,0 +1,18 @@
+/*
+ *DESCRIPTION
+ *   Calculate a + b
+ *INPUT:
+ *   Two integer a,b (0 ¡Ü a,b ¡Ü 10)
+ *OUTPUT:
+ *   Output a + b
+ */
+
+#include <iostream>
+using namespace std;
+int  main()
+{
+    int a,b;
+    cin >> a >> b;
+    cout << a+b << endl;
+    return 0;
+}
